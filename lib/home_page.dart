96155881ac1c78c2a83f28182339c32b41421692
IchsanNurRachmadY <ichsannurrachmad7@gmail.com>
2023-03-31 14:38:41 +0700

    first comit

---
+++ lib/home_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Widget kotakmerah() {
+      return Container(
+        height: 80,
+        width: 80,
+        margin: const EdgeInsets.only(right: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(17),
+          color: Colors.red,
+        ),
+      );
+    }
+
+    Widget kotakhijau() {
+      return Container(
+        height: 160,
+        width: 160,
+        margin: const EdgeInsets.only(right: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(17),
+          color: Colors.green,
+        ),
+      );
+    }
+
+    Widget kotakbiru() {
+      return Container(
+        height: 160,
+        width: double.infinity,
+        margin: const EdgeInsets.only(top: 20),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(17),
+          color: Colors.blue,
+        ),
+      );
+    }
+
+    return Scaffold(
+      backgroundColor: Color(0xffF4F4F4),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: Column(children: [
+            SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  kotakmerah(),
+                  kotakmerah(),
+                  kotakmerah(),
+                  kotakmerah(),
+                  kotakmerah(),
+                  kotakmerah(),
+                ],
+              ),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(children: [
+                  kotakhijau(),
+                  kotakhijau(),
+                  kotakhijau(),
+                ])),
+            SizedBox(
+              height: 5,
+            ),
+            kotakbiru(),
+            kotakbiru(),
+            kotakbiru(),
+            kotakbiru(),
+          ]),
+        ),
+      ),
+    );
+  }
+}
